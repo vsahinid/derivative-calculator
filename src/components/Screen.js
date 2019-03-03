231b48e07567aee8f89bfedd6b3cd7aff904bebd
vsahinid <vsahinid@alumni.cmu.edu>
2019-03-03 14:14:02 -0600

    Added app to home screen

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Button } from 'reactstrap';
+import React from "react";
+import { Button } from "reactstrap";
 
-const Screen = (props) => {
-    return (
-        <div onClick={props.functionPassed}>
-            <Button color="secondary" style={{ width: "475px", height: "50px", margin: "2.5px" }} disabled>{props.children}</Button>
-        </div>
-    );
+const Screen = props => {
+  return (
+    <div onClick={props.functionPassed}>
+      <Button
+        color="secondary"
+        style={{ width: "475px", height: "50px", margin: "2.5px" }}
+        disabled
+      >
+        {props.children}
+      </Button>
+    </div>
+  );
 };
 
 export default Screen;
